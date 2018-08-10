1c2a081f132e79847ba4c3727e6b363ca82d811d
Andrea Vázquez Garay <andrea@MacBook-Pro-de-Andrea.local>
2018-08-09 19:16:21 -0700

    Basic Programing

---
+++ 08-07-Actividad/Suma.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Agosto2018
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int[] arreglo = new int[] { 4, 5, 6, 9, 7 };
+			int suma= Sumar(arreglo);
+			int Min= Minimo(arreglo);
+            int Max= Maximo(arreglo);
+            Console.WriteLine("La suma del arreglo es: " + suma);
+            Console.WriteLine("Mínimo: " + Min);
+            Console.WriteLine("Máximo: " + Max);
+
+		}
+
+
+		private static int Sumar(int[] arreglo)
+		{
+			int suma = 0;
+
+			for (int i = 0; i < arreglo.Length; i++)
+			{
+				suma += arreglo[i];
+			}
+            return suma;
+		}
+
+		private static int Minimo(int[] arreglo)
+		{
+			int Min = arreglo[0];
+
+			for (int i = 0; i < arreglo.Length; i++)
+			{
+				if (arreglo[i] < Min)
+				{
+					Min = arreglo[i];
+				}
+			}
+            return Min;
+
+		}
+
+		private static int Maximo(int[] arreglo)
+		{
+			int Max = arreglo[0];
+
+			for (int i = 0; i < arreglo.Length; i++)
+			{
+				if (arreglo[i] > Max)
+				{
+					Max = arreglo[i];
+				}
+
+			}
+            return Max;
+
+			
+		}
+	}
+}
---
+++ 08-09-Actividad/NumerosPrimos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace agosto2018
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] arreglo = new int[] { 6, 3, 9, 5, 2 };
+            bool primo = false;
+
+            if(arreglo[0]%arreglo[0]==1)
+            {
+                primo = true;
+            }
+            Console.WriteLine(primo);
+        }
+    }
+}
