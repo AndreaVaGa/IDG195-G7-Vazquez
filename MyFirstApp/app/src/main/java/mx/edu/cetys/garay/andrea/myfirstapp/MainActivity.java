261583c95ad5e5f4ca6b636693ad221d69c34236
Andrea Vázquez Garay <andrea@MacBook-Pro-de-Andrea.local>
2018-08-28 14:28:42 -0700

    App

--- MyFirstApp/app/src/main/java/mx/edu/cetys/garay/andrea/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/mx/edu/cetys/garay/andrea/myfirstapp/MainActivity.java
@@ -1,13 +1,17 @@
 package mx.edu.cetys.garay.andrea.myfirstapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -17,19 +21,61 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btnconsultar, btnGuardar;
+    EditText etId, etNombre, etTelefono;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        btnconsultar = (Button) findViewById(R.id.btnConsultar);
+        btnGuardar = (Button)  findViewById(R.id.btnGuardar);
+        etId = (EditText) findViewById(R.id.etId);
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        etTelefono = (EditText) findViewById(R.id.etTel);
+
+        btnconsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ConsultarDatos().execute("http://138.68.231.116:5000/prueba");
+                VolleyRequest();
+
+
+
+            }
+        });
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CargarDatos().execute("http://138.68.231.116:5000/pruebanombre="+etNombre.getText().toString()+"&Tel="+etTelefono.getText().toString());
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 VolleyRequest();
                 Snackbar.make(view, "Hola Amiguitos idgdeanos", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
@@ -38,9 +84,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void VolleyRequest(){
+    public void VolleyRequest() {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://api.myjson.com/bins/i15p8";
+        String url = "http://138.68.231.116:5000/prueba";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -50,11 +96,12 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),
                                 response.substring(0, 500),
                                 Toast.LENGTH_LONG).show();
+
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-               Toast.makeText(getApplicationContext(), error.getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -82,5 +129,112 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private class CargarDatos extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), "se almacenaran los datos correctamente", Toast.LENGTH_LONG).show();
+
+
+        }
+    }
+
+    private class ConsultarDatos extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            JSONArray array= null;
+            try {
+                array = new JSONArray(result);
+
+                etNombre.setText(array.getString(1));
+                etTelefono.setText(array.getString(2));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            myurl = myurl.replace(" ", "%20");
+
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+
+                conn.setDoInput(true);
+
+                conn.setRequestMethod("GET");
+                conn.setDoOutput(true);
+                conn.setChunkedStreamingMode(0);
+                OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+                out.close();
+
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d("respuesta", "The response is: " + response);
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+
+
+        }
+
+    public String readIt(InputStream stream, int maxReadSize) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] rawBuffer = new char[maxReadSize];
+        int readSize;
+        StringBuffer buffer = new StringBuffer();
+        while (((readSize = reader.read(rawBuffer)) != -1) && maxReadSize > 0) {
+            if (readSize > maxReadSize) {
+                readSize = maxReadSize;
+            }
+            buffer.append(rawBuffer, 0, readSize);
+            maxReadSize -= readSize;
+        }
+        return buffer.toString();
     }
 }
