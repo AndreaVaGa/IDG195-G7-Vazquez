db0868338cbf67ab35ead4c4c4d868ecfafe337a
Shamira03 <macias3roa@gmail.com>
2018-09-11 20:00:46 -0700

    Tutores

---
+++ Micampus/app/src/main/java/mx/edu/cetys/garay/andrea/micampus/view/TutoresViewActivity.kt
@@ -0,0 +1,13 @@
+package mx.edu.cetys.garay.andrea.micampus.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.cetys.garay.andrea.micampus.R.layout.activity_tutores_view
+
+class TutoresViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_tutores_view)
+    }
+}
