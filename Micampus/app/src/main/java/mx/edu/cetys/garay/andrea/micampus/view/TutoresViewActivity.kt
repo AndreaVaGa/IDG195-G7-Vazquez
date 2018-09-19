b9fde425968f55dac7ae0f727bf08be4037bb8e5
Angel Arambula <angel.arambula@thermofisher.com>
2018-09-18 18:44:56 -0700

    Added toolbar and fixed imports

--- Micampus/app/src/main/java/mx/edu/cetys/garay/andrea/micampus/view/TutoresViewActivity.kt
+++ Micampus/app/src/main/java/mx/edu/cetys/garay/andrea/micampus/view/TutoresViewActivity.kt
@@ -2,7 +2,11 @@ package mx.edu.cetys.garay.andrea.micampus.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_tutores.*
 import kotlinx.android.synthetic.main.content_tutores.*
+import mx.edu.cetys.garay.andrea.micampus.R
 import mx.edu.cetys.garay.andrea.micampus.R.layout.activity_tutores
 import mx.edu.cetys.garay.andrea.micampus.model.Alumno
 
@@ -12,7 +16,7 @@ class TutoresViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(activity_tutores)
 
-        //setActionBar(findViewById(R.id.tutores_toolbar))
+        setSupportActionBar(tutores_toolbar)
 
         val perfil = intent.getParcelableExtra<Alumno>("tutor")
 
@@ -28,5 +32,21 @@ class TutoresViewActivity : AppCompatActivity() {
         tv_txt_correom.text = perfil.madre.correo
         tv_txt_direccionm.text = perfil.madre.direccion
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
