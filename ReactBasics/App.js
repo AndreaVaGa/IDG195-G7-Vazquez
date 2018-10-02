e880ba90cb5ab9d8afce96e0a4f8c11ec74350ea
Andrea Vázquez Garay <andrea@MacBook-Pro-de-Andrea.local>
2018-10-02 12:05:55 -0700

    React Basics, image addition with url ussing a prop

--- ReactBasics/App.js
+++ ReactBasics/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = { uri: 'https://i2.wp.com/thehappening.com/wp-content/uploads/2016/02/boliche-cdmx.jpg?fit=1024%2C694&ssl=1'};
     return (
       <View style={styles.container}>
         <Text>Hello World!</Text>
+        <Image source={pic} style={{width: 240, height: 140}}/>
       </View>
     );
   }
