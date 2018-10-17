770409bb88a89e541f5b6338cd5643418b3977a6
Luis <bookmagician>
2018-10-17 16:44:08 -0700

    Menu Design - Front End

--- MiCampusReact/Ventanas/Menu.js
+++ MiCampusReact/Ventanas/Menu.js
@@ -4,18 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class Menu extends React.Component {
   render() {
     return (
+      <View>
+        <View style={styles.container}>
+          <Text>Academico</Text>
+        </View>
 
-      <View style={styles.container}>
-        <Text>Academico</Text>
-      </View>
-
+        <Image source={require('./src/imgs/background.jpg')}>
+          style={styles.backgroundImage}>
+          {this.props.children}
+        </Image>
+      </View>  
     );
   }
 }
 
 class BackgroundImage extends React.Component {
   render() {
-    <Image source={require('../src/imgs/background.jpg')}>
+    <Image source={require('./src/imgs/background.jpg')}>
       {this.props.children}
     </Image>
   }
