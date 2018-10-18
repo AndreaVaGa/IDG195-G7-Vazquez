5ac7e2a0ddd6e9471a0a650e59e82ededf55f67f
Luis <bookmagician>
2018-10-18 08:44:27 -0700

    Menu Design - Front End

--- MiCampusReact/Ventanas/Menu.js
+++ MiCampusReact/Ventanas/Menu.js
@@ -7,17 +7,35 @@ export default class Menu extends React.Component {
       <View>
         <ImageBackground style={styles.backgroundImage} source={require('../src/imgs/background.jpg')}>
         </ImageBackground>
+        <Text style={style.title}>Académico</Text>
+        <View style={style.container}>
+          <Button onPress={() => {alert.alert('Tapped Academico')}}>
+            <Image style={style.icon} source={require("../src/imgs/historial.png")}></Image>
+          </Button>
+          <Button onPress={() => {alert.alert('Tapped Academico')}}>
+            <Image style={style.icon} source={require("../src/img/boleta.png")}></Image>
+          </Button>
+        </View>
       </View>  
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black'
+  container:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-between'
+  },
+  icon:{
+    width: 100,
+    heigth: 100,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    color: 'white'
   },
   backgroundImage: {
     width: screenWidth,
