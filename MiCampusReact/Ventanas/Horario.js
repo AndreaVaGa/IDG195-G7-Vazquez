4f7725aee9bffadb351d20c3406f870a4c29760b
IvanniaEdith <ivanniaedith@gmail.com>
2018-10-18 21:18:00 -0700

    COrreccion completa del codigo en Horario

--- MiCampusReact/Ventanas/Horario.js
+++ MiCampusReact/Ventanas/Horario.js
@@ -1,74 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-export default class Horario extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usuario: '',
-      password: ''
-    };
-  }
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-  _getAlumnosFromApiAsync=()=> {
-    return fetch('http://138.68.231.116:5000/alumnos')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        var usuario = this.state.usuario
-        var password = this.state.password
-        
-        var test = responseJson.find(function (obj) { return obj.matricula === usuario && obj.password === password});
-        return test;
-      })
-      .then((object) => {
-        if (object === undefined) {
-          alert('Mi matricula lleva 0 compa')
-        } else {
-          alert(object.matricula + ' ' + object.nombre)
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
+type Props = {};
+export default class Horario extends Component<Props> {
   render() {
     return (
-      <View style={{ padding: 20 }}>
-        <TextInput
-          style={{ height: 60 }}
-          placeholder="Matricula"
-          onChangeText={(usuario) => this.setState({ usuario })}
-        />
-        <TextInput
-          style={{ height: 60 }}
-          placeholder="Contraseña"
-          onChangeText={(password) => this.setState({ password })}
-        />
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._getAlumnosFromApiAsync}
-            title="Ingresar"
-          //color="#000"
-          />
+      <View style={styles.container}>
+        <View style={styles.fila}>
+          <View style={styles.materia}>
+            <Text style={styles.headers}>Base de Datos</Text>
+            <Text style={styles.profesor}>Gerardo del Rincon</Text>
+          </View>
+          <View style={styles.salon}>
+            <Text style={styles.numero}>7013</Text>
+            <Text style={styles.hora}>6:00 - 8:00 p.m.</Text>
+          </View>
         </View>
       </View>
     );
-
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1,  
   },
-  buttonContainer: {
-    margin: 20,
-  },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
+  fila: {
+    marginTop: 30,
+    marginLeft: 22,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+  },
+  materia: {
+    width: 240,
+    height: 100,
+    justifyContent:'center',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    backgroundColor: '#F5F5F5',
+    flexDirection: 'column',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+  salon: {
+    width: 80,
+    height: 100,
+    justifyContent:'center',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: '#ffffff',
+    flexDirection: 'column',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+  headers: {
+    fontSize: 20,
+    textAlign: 'left',
+    marginLeft: 10,
+  },
+  numero: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  profesor: {
+    textAlign: 'left',
+    color: '#333333',
+    marginTop: 2,
+    marginLeft: 10,
+  },
+  hora: {
+    textAlign: 'center',
+    color: '#333333',
+    marginTop: 2,
+    fontSize: 10,
   }
-});
+ });
