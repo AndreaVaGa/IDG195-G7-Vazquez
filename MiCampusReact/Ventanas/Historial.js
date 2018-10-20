3654599e13e3572d45b731bcdd27f60a3d23dca8
Luis <bookmagician>
2018-10-20 00:22:35 -0700

    Historial - Front End Design

--- MiCampusReact/Ventanas/Historial.js
+++ MiCampusReact/Ventanas/Historial.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 
 export default class Historial extends React.Component {
   render() {
@@ -8,21 +8,54 @@ export default class Historial extends React.Component {
         <View>
             <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 20 }}>Historial Académico</Text>
         </View>
-        <View>
-          <Text style={{color:'black', fontSize:15}}>Promedio general</Text>
-        </View>
 
         <View style={styles.row}>
           <View style={styles.colorBox1}>
           </View>
           <View style={styles.materia}>
-            <Text>Cursando</Text>
+            <Text style={styles.headers}>Cursando</Text>
+          </View>
+          <View style={styles.rowIcon}>
+            <Image source={require('../src/imgs/dropdown-01.png')}></Image>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View style={styles.colorBox2}>
+          </View>
+          <View style={styles.materia}>
+            <Text style={styles.headers}>Puedes Cursar</Text>
+          </View>
+          <View style={styles.rowIcon}>
+            <Image source={require('../src/imgs/dropdown-01.png')}></Image>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View style={styles.colorBox3}>
+          </View>
+          <View style={styles.materia}>
+            <Text style={styles.headers}>Aprobadas</Text>
+          </View>
+          <View style={styles.rowIcon}>
+            <Image source={require('../src/imgs/dropdown-01.png')}></Image>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View style={styles.colorBox4}>
+          </View>
+          <View style={styles.materia}>
+            <Text style={styles.headers}>Sujetas a Aprobación</Text>
           </View>
           <View style={styles.rowIcon}>
             <Image source={require('../src/imgs/dropdown-01.png')}></Image>
           </View>
         </View>
         
+        <View>
+          <Text style={{color:'black', fontSize:15}}>Promedio general</Text>
+        </View>
       </View>
     );
   }
@@ -59,12 +92,58 @@ const styles = StyleSheet.create({
     backgroundColor:'#4481c2',
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  colorBox2:{
+    width: 65,
+    height: 100,
+    backgroundColor:'#87c540',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  colorBox3:{
+    width: 65,
+    height: 100,
+    backgroundColor:'#fdd900',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  colorBox4:{
+    width: 65,
+    height: 100,
+    backgroundColor:'#f78d1f',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
   },
   rowIcon:{
-    width:65,
-    height:100,
-    borderTopRightRadius:10,
-    borderBottomRightRadius: 10,
+    promedio: {
+      width: 65,
+      height: 100,
+      borderTopRightRadius: 10,
+      borderBottomRightRadius: 10,
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+      backgroundColor: '#F5F5F5',
+      flexDirection: 'column',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.8,
+      shadowRadius: 2,
+      elevation: 4,
+      alignContent:'center',
+    },
+  },
+  headers: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 15,
   },
   text: {
     fontSize: 20,
