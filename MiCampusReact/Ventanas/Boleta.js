d4e802a5ec351e8fe77067d7bec405a5bcf9831a
Shamira03 <macias3roa@gmail.com>
2018-10-23 17:27:37 -0700

    Acomodo info boleta con diseño

--- MiCampusReact/Ventanas/Boleta.js
+++ MiCampusReact/Ventanas/Boleta.js
@@ -4,13 +4,12 @@ import {
   Text,
   View,
   FlatList,
-  AsyncStorage,
-  SearchBar
+  AsyncStorage
 } from 'react-native';
 
 
 class ListViewDemo extends React.Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
@@ -28,76 +27,101 @@ class ListViewDemo extends React.Component {
     var value = await AsyncStorage.getItem('boleta');
     if (value !== undefined) {
       var boleta = JSON.parse(value)
-      this.setState({data: boleta.boleta})
-
-      //this.setState({ promedio: historial.PromedioGeneral })
+      this.setState({ data: boleta.boleta })
     }
-  }
-  
-
-ListViewItemSeparator = () => {
-    return (
-      <View
-        style={{
-          height: .5,
-          width: "100%",
-          backgroundColor: "#000",
-        }}
-      />
-    );
   }
 
   render() {
     return (
       <View style={styles.container} >
-        <Text style={styles.h2text}>
-          Boleta
-        </Text>
         <FlatList
           data={this.state.data}
           showsVerticalScrollIndicator={false}
-          renderItem={({item}) =>
-          <View style={styles.flatview}>
-            <Text style={styles.name}>{item.materia}</Text>
-            <Text style={styles.email}>{item.profesor}</Text>
-            <Text>{item.calif}</Text>
-            <Text>{item.faltas}</Text>
-          </View>
+          renderItem={({ item }) =>
+            <View style={styles.fila}>
+
+              <View style={styles.materia}>
+                <Text style={styles.headers}>{item.materia}</Text>
+                <Text style={styles.texto}>{item.profesor}</Text>
+              </View>
+
+              <View style={styles.faltas}>
+                <Text style={styles.headers}>F</Text>
+                <Text style={styles.texto}>{item.faltas}</Text>
+              </View>
+
+              <View style={styles.promedio}>
+                <Text style={styles.headers}>P</Text>
+                <Text style={styles.texto}>{item.calif}</Text>
+              </View>
+
+            </View>
           }
           keyExtractor={item => item.materia}
-          ItemSeparatorComponent={this.ListViewItemSeparator} 
+          ItemSeparatorComponent={this.ListViewItemSeparator}
         />
       </View>
     );
   }
-  
+
 }
 export default ListViewDemo;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginBottom: 20
   },
-  h2text: {
-    marginTop: 10,
-    fontSize: 36,
-    fontWeight: 'bold',
+  fila: {
+    marginTop: 30,
+    marginLeft: 22,
+    flexDirection: 'row',
   },
-  flatview: {
-    justifyContent: 'center',
-    paddingTop: 30,
-    borderRadius: 2,
+  materia: {
+    width: 200,
+    height: 100,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    backgroundColor: '#F5F5F5',
+    flexDirection: 'column',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
   },
-  name: {
-    fontFamily: 'Verdana',
-    fontSize: 18
+  faltas: {
+    width: 65,
+    height: 100,
+    backgroundColor: '#ffffff',
+    flexDirection: 'column',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
   },
-  email: {
-    color: 'green'
+  promedio: {
+    width: 65,
+    height: 100,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: '#ffffff',
+    flexDirection: 'column',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 4,
+  },
+  headers: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 15,
+  },
+  texto: {
+    textAlign: 'center',
+    color: '#333333',
+    marginTop: 5,
   }
-
-});
-  
+ });
+ 
