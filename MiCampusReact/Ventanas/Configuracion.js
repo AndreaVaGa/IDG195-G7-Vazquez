f809452491eadcaa135e912ce327fcec0490754f
Luis <bookmagician>
2018-11-08 19:33:16 -0800

    Configuracion - Front End

--- MiCampusReact/Ventanas/Configuracion.js
+++ MiCampusReact/Ventanas/Configuracion.js
@@ -7,6 +7,26 @@ export default class Configuracion extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Configuración</Text>
+
+        <View style={style.textBox}>
+        <Text style={styles.titles}>Notificaciones</Text>
+        </View>
+
+        <View style={style.textBox}>
+        <Text style={styles.titles}>Fondo</Text>
+        </View>
+
+        <View style={style.textBox}>
+        <Text style={styles.titles}>Terminos/Privacidad</Text>
+        </View>
+
+        <View style={style.button}>
+          <Button
+            title="Log Out"
+            color="#FFFFFF"
+            onPress={(this._LogOut)}
+            accessibilityLabel="Tap on Me" />
+        </View>
       </View>
     );
   }
@@ -23,5 +43,23 @@ const styles = StyleSheet.create({
     padding: 10,
     color: 'black'
   },
+
+  textBox:{
+    backgroundColor: '#efefef',
+    borderRadius: 20,
+    margin: 20
+  },
+  
+  titles:{
+    fontSize: 20,
+    textAlign: 'left',
+    marginTop: 15,
+  },
+
+  button: {
+    backgroundColor: '#ec1d23',
+    borderRadius: 10,
+    margin: 20
+  }
 
 });
