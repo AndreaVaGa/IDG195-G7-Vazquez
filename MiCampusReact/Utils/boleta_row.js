52d70f0875c271da288caaa59154ebabbd4283cd
Daniela Agundez <danielaagundez@Danielas-MacBook-Pro-3.local>
2018-11-21 16:41:45 -0800

    Moved collapsed to component state

---
+++ MiCampusReact/Utils/boleta_row.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+} from 'react-native';
+
+class BoletaRow extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            collapsed: true
+        };
+    }
+    _changeHeight = () => {
+        if (this.state.collapsed) {
+            this._height.setNativeProps({ height: 150 });
+        }
+        else {
+            this._height.setNativeProps({ height: 100 });
+        }
+        this.state.collapsed = !this.state.collapsed;
+    }
+
+    //se llama la funcion desde el renglon individualmente
+    _onPress = () => {
+        this._changeHeight()
+        //this.props.onPressItem(this.props.materia);
+    };
+
+    render() {
+        return (
+            <TouchableOpacity onPress={this._onPress}>
+                <View style={styles.fila}>
+                    <View style={styles.materia} ref={component => this._height = component}>
+                        <Text style={styles.headers}>{this.props.materia}</Text>
+                        <Text style={styles.texto}>{this.props.profesor}</Text>
+                    </View>
+                    <View style={styles.faltas} ref={component => this._height = component}>
+                        <Text style={styles.headers}>F</Text>
+                        <Text style={styles.texto}>{this.props.faltas}</Text>
+                    </View>
+                    <View style={styles.promedio} ref={component => this._height = component}>
+                        <Text style={styles.headers}>P</Text>
+                        <Text style={styles.texto}>{this.props.calif}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
+
+export default BoletaRow;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginBottom: 20
+    },
+    fila: {
+        marginTop: 30,
+        marginLeft: 22,
+        flexDirection: 'row',
+    },
+    materia: {
+        width: 200,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        backgroundColor: '#F5F5F5',
+        flexDirection: 'column',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    faltas: {
+        width: 65,
+        backgroundColor: '#ffffff',
+        flexDirection: 'column',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    promedio: {
+        width: 65,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        backgroundColor: '#ffffff',
+        flexDirection: 'column',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 4,
+    },
+    headers: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 15,
+    },
+    texto: {
+        textAlign: 'center',
+        color: '#333333',
+        margin: 5
+    }
+});
--- MiCampusReact/Ventanas/Boleta.js
+++ MiCampusReact/Ventanas/Boleta.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
   FlatList,
   AsyncStorage,
-  ScrollView,
-  Switch,
-  TouchableOpacity
 } from 'react-native';
-
-var collapsed = true;
+import BoletaRow from '../Utils/boleta_row';
 
 class ListViewDemo extends React.Component {
 
@@ -24,9 +19,11 @@ class ListViewDemo extends React.Component {
       refreshing: false,
     };
   }
+
   componentDidMount() {
     this._loadInitionState().done();
   }
+
   _loadInitionState = async () => {
     var value = await AsyncStorage.getItem('boleta');
     if (value !== undefined) {
@@ -34,123 +31,44 @@ class ListViewDemo extends React.Component {
       this.setState({ data: boleta.boleta })
     }
   }
+  //funcion que se define como atributo y se puede mandar 
+  //a ejecutar desde el componente
+  _onPressItem = (value) => {
+    alert(value)
+  };
 
-  changeHeight = () => {
-
-    if (collapsed) {
-      this._height.setNativeProps({ height: 150 });
-      this._height2.setNativeProps({ height: 150 });
-      this._height3.setNativeProps({ height: 150 });
-    }
-    else {
-      this._height.setNativeProps({ height: 100 });
-      this._height2.setNativeProps({ height: 100 });
-      this._height3.setNativeProps({ height: 100 });
-    }
-
-    collapsed = !collapsed;
-  }
+  _renderItem = ({ item }) => (
+    <BoletaRow
+      onPressItem={this._onPressItem}
+      matricula={item.matricula}
+      materia={item.materia}
+      calif={item.calif}
+      profesor={item.profesor}
+      faltas={item.faltas}
+    />
+  );
 
   render() {
     return (
       <View style={styles.container} >
-
-
         <FlatList
           data={this.state.data}
+          extraData={this.state}
+          keyExtractor={(item, index) => item.materia}
+          renderItem={this._renderItem}
           showsVerticalScrollIndicator={false}
-          renderItem={({ item }) =>
-
-
-            <TouchableOpacity onPress={this.changeHeight}>
-
-
-              <View style={styles.fila}>
-
-                <View style={styles.materia} ref={component => this._height = component}>
-                  <Text style={styles.headers}>{item.materia}</Text>
-                  <Text style={styles.texto}>{item.profesor}</Text>
-                </View>
-
-                <View style={styles.faltas} ref={component => this._height2 = component}>
-                  <Text style={styles.headers}>F</Text>
-                  <Text style={styles.texto}>{item.faltas}</Text>
-                </View>
-
-                <View style={styles.promedio} ref={component => this._height3 = component}>
-                  <Text style={styles.headers}>P</Text>
-                  <Text style={styles.texto}>{item.calif}</Text>
-                </View>
-              </View>
-
-
-            </TouchableOpacity>
-
-
-          }
-          keyExtractor={item => item.materia}
-          ItemSeparatorComponent={this.ListViewItemSeparator}
         />
-
       </View>
     );
   }
-
 }
+
 export default ListViewDemo;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginBottom: 20
-  },
-  fila: {
-    marginTop: 30,
-    marginLeft: 22,
-    flexDirection: 'row',
-  },
-  materia: {
-    width: 200,
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-    backgroundColor: '#F5F5F5',
-    flexDirection: 'column',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 4,
-  },
-  faltas: {
-    width: 65,
-    backgroundColor: '#ffffff',
-    flexDirection: 'column',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 4,
-  },
-  promedio: {
-    width: 65,
-    borderTopRightRadius: 10,
-    borderBottomRightRadius: 10,
-    backgroundColor: '#ffffff',
-    flexDirection: 'column',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 4,
-  },
-  headers: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 15,
-  },
-  texto: {
-    textAlign: 'center',
-    color: '#333333',
-    margin: 5
   }
 });
 
